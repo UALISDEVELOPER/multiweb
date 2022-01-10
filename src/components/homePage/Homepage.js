a78f333a571cff64b2f87bc89f5302a4a4ab1d2f
Ali Ghorbani <alighorbani828282@gmail.com>
2022-01-10 21:11:43 +0330

    website is SPA now

--- src/components/homePage/Homepage.js
+++ src/components/homePage/Homepage.js
@@ -1,19 +1,25 @@
 import React from 'react';
 
+
+//bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container} from 'react-bootstrap';
+
 //components
 import CarouselComponent from './Carousel';
 import FirstRow from './FirstRow';
 import Devices from './Devices';
 import SeconRow from './SeconRow';
 
+
 const Homepage = () => {
     return (
-        <div>
+        <Container fluid>
             <CarouselComponent/>
             <FirstRow />
             <Devices/>
             <SeconRow/>
-        </div>
+        </Container>
     );
 };
 
