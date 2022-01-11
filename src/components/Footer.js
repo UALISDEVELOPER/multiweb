44d3cc8cf77b2ad866eadd9c0ca1d985ff0ad112
Ali Ghorbani <alighorbani828282@gmail.com>
2022-01-11 17:49:06 +0330

    footer added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
+//bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container,Row , Col} from "react-bootstrap"
+
+//styles
+import "./footer.scss"
+
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <Container fluid className='footer'>
+            <Row>
+                <Col>
+                    <p>Crated by <a href="mailto:03alighorbani@gmail.com">Ali Ghorbani</a></p>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
